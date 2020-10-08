2f3fad4a1cf3da800287220aea9e8eedc86fe432
tljohnsn <tljohnsn@bankbook1.local>
2020-10-08 06:53:45 -0500

    save data

---
+++ scripts/savedata.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+systemctl stop apt-daily.timer
+systemctl disable apt-daily.timer
+systemctl mask apt-daily.service
+
+systemctl stop apt-daily-upgrade.timer
+systemctl disable apt-daily-upgrade.timer
+systemctl mask apt-daily-upgrade..service
+
+sed -i -e "s/\*:09,39/1:09/" /etc/systemd/system/timers.target.wants/phpsessionclean.timer
+
+systemctl daemon-reload
+
+echo '# redirect rngd output on Raspberry Pi Buster
+:programname,startswith,"rng" /dev/null
+:programname,startswith,"rng" stop
+'>/etc/rsyslog.d/rng.conf
