286ec47880d5a7ecaf637dc158a6d726fbe316e7
Your Name <you@example.com>
2021-07-05 16:13:46 -0500

    iris autoconf

--- configfiles/index.php
+++ configfiles/index.php
@@ -1,6 +1,6 @@
 <?php
 echo '<font size=7><br>';
-echo '<a href=http://' .$_SERVER["HTTP_HOST"] . ':6680/iris/library/artists>Iris</a><br><br>';
+echo '<a href=http://' .$_SERVER["HTTP_HOST"] . ':6680/iris/library/artists>Iris/?ui={"initial_setup_complete":true}</a><br><br>';
 echo '<a href=http://' .$_SERVER["HTTP_HOST"] . '/ampache/#browse.php?action=artist>Ampache</a><br><br>';
 echo '<a href=http://' .$_SERVER["HTTP_HOST"] . '/rompr/>RompR</a><br><br>';
 echo '</font>';
