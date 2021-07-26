16f77552d740ed0c7c535867df47facad10cb694
Trent Johnson <tljohnsn@MacMini.local>
2021-07-26 09:24:06 -0500

    sync

--- scripts/syncssh.sh
+++ scripts/syncssh.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-rsync='rsync -avP --inplace --exclude=.sync --exclude=.DS_Store --exclude=._*'
-echo $rsync
-$rsync -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" /Music/convertedflacs/. pi@tunes.local:/home/ftp/local/convertedflacspi/.
-$rsync -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" /Music/mp3zlaptop/. pi@tunes.local:/home/ftp/local/mp3zpi/.
+src="/Music"
+dst="pi@tunes.local:/home/ftp/local"
+rsync='rsync -avP --exclude=.sync --exclude=.DS_Store --exclude=._* --delete'
+#echo $rsync
+
+$rsync -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $src/convertedflacs/. $dst/convertedflacspi/.
+$rsync -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $src/mp3zlaptop/. $dst/mp3zpi/.
+
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null pi@tunes.local sudo /etc/cron.daily/mopidylocalscan
