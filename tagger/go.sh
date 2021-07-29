e7f7084e4789c00813482aa8bb1d867f10f4fd48
Trent Johnson <tljohnsn@trentbook4.local>
2021-07-29 07:42:48 -0500

    publish

--- tagger/go.sh
+++ tagger/go.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+cd ~/piplayer/tagger
+git pull
+cp ~/piplayer/tagger/config.yaml ~/.config/beets
+ 
 rm -rf ~/mp3zrenamed/* ~/data/musiclibrary.db
 beet import -w -c ~/mp3zstaging
 find  ~/mp3zrenamed -name "*.mp3" -print0 | xargs -0 mid3v2 --delete-frames=TXXX:ALBUMARTISTSORT,TSOP,TSOC
 find ~/mp3zrenamed -name cover.?.jpg -print0 |xargs -0 rm
+
+echo "now run:"
+echo " ~/piplayer/tagger/syncssh.sh"
