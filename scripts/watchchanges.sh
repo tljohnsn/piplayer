b473687f7752274fc4290db9083d7f9a1fdec3ed
Your Name <you@example.com>
2021-08-03 10:09:33 -0500

    while around loop

--- scripts/watchchanges.sh
+++ scripts/watchchanges.sh
@@ -3,16 +3,19 @@ NOW=`date +'%s'`
 LASTRUN=`date +'%s'`
 PAST=`date +'%s'`
 echo starting $NOW
-while mpc idle database; do
-    NOW=`date +'%s'`
-    RUNDURATION=`expr $NOW - $LASTRUN`
-    CHANGEDURATION=`expr $NOW - $PAST`
-    echo it changed NOW: $NOW LASTRUN: $LASTRUN RUNDURATION: $RUNDURATION PAST: $PAST CHANGEDURATION: $CHANGEDURATION
-    ~/piplayer/scripts/update-rompr.sh
+while [ 1 == 1 ]; do
+    echo "Starting idle loop"
+    while mpc idle database; do
+	NOW=`date +'%s'`
+	RUNDURATION=`expr $NOW - $LASTRUN`
+	CHANGEDURATION=`expr $NOW - $PAST`
+	echo it changed NOW: $NOW LASTRUN: $LASTRUN RUNDURATION: $RUNDURATION PAST: $PAST CHANGEDURATION: $CHANGEDURATION
+	~pi/piplayer/scripts/update-rompr.sh
 #    if [[ ( $RUNDURATION -gt 10 ) && ( $CHANGEDURATION -gt 10 ) ]]; then
 #	echo Rebuild the index
 #	sleep 11
 #	LASTRUN=`date +'%s'`
 #    fi
-    PAST=`date +'%s'`
+	PAST=`date +'%s'`
+    done
 done
