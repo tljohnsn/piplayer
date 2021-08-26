66236b0a623d32f656b21ab9914ddbd63aee4d66
Trent Johnson <tljohnsn@MacMini.local>
2021-08-26 12:16:14 -0500

    update

--- tagger/go.sh
+++ tagger/go.sh
@@ -5,6 +5,7 @@ cp ~/piplayer/tagger/config.yaml ~/.config/beets
  
 rm -rf ~/mp3zrenamed/* ~/data/musiclibrary.db
 beet import -w -c ~/mp3zstaging
+beet bpmanalyser bpm:0
 find  ~/mp3zrenamed -name "*.mp3" -print0 | xargs -0 mid3v2 --delete-frames=TXXX:ALBUMARTISTSORT,TSOP,TSOC
 find ~/mp3zrenamed -name cover.?.jpg -print0 |xargs -0 rm
 
--- tagger/pushtest.sh
+++ tagger/pushtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync='rsync -avP --inplace --exclude=.sync --exclude=.DS_Store --exclude=._* --delete'
+rsync='rsync -avP --inplace --no-W --exclude=.sync --exclude=.DS_Store --exclude=._* --delete'
 echo $rsync
 $rsync /musictest/convertedflacs/. /Music/convertedflacs/.
 $rsync /musictest/mp3zlaptop/. /Music/mp3zlaptop/.
