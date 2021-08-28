2a44491ed879875d2134d55f19cf573b47dd5cd6
tljohnsn <tljohnsn@bankbook1.local>
2021-08-28 07:45:54 -0500

    inplace

--- tagger/synctesh.sh
+++ tagger/synctesh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync='rsync -avP --inplace --no-W --exclude=.sync --exclude=.DS_Store --exclude=._* --delete'
+rsync='rsync -avP --exclude=.sync --exclude=.DS_Store --exclude=._* --delete'
 echo $rsync
 $rsync /Music/convertedflacs/. /musictest/convertedflacs/.
 $rsync /Music/mp3zlaptop/. /musictest/mp3zlaptop/.
