8d33878b8cde7f6f0e517f57bdabb2d4b86cb320
Your Name <you@example.com>
2021-10-28 14:27:24 -0400

    excludes

--- scripts/syncmusic.sh
+++ scripts/syncmusic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avP --delete /Music/convertedflacs/. /Volumes/public/convertedflacspi/.
-rsync -avP --delete /Music/mp3zlaptop/. /Volumes/public/mp3zpi/.
+rsync -avP --delete --exclude=.sync --exclude=.DS_Store --exclude=._* /Music/convertedflacs/. /Volumes/public/convertedflacspi/.
+rsync -avP --exclude=.sync --exclude=.DS_Store --exclude=._* --delete /Music/mp3zlaptop/. /Volumes/public/mp3zpi/.
