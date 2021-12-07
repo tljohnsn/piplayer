d9296dee45811b25b7c80cb63e7ac471b3466e5c
tljohnsn <tljohnsn@bankbook1.local>
2021-12-07 06:58:06 -0500

    Fix bpm at import

--- tagger/go.sh
+++ tagger/go.sh
@@ -2,10 +2,16 @@
 cd ~/piplayer/tagger
 git pull
 cp ~/piplayer/tagger/config.yaml ~/.config/beets
- 
+
+echo "remove old music and database" 
 rm -rf ~/mp3zrenamed/* ~/data/musiclibrary.db
+echo "begin the import"
 beet import -w -c ~/mp3zstaging
-beet bpmanalyser bpm:0
+echo "alalyse bpm"
+beet bpmanalyser -w bpm:0
+echo "writing bpm"
+beet write
+echo "remove sort tags"
 find  ~/mp3zrenamed -name "*.mp3" -print0 | xargs -0 mid3v2 --delete-frames=TXXX:ALBUMARTISTSORT,TSOP,TSOC
 find ~/mp3zrenamed -name cover.?.jpg -print0 |xargs -0 rm
 
