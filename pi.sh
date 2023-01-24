1279687addd0c79a980b5ef33271b94c6e73a217
tljohnsn <tljohnsn@macbook2020.mhs>
2023-01-24 07:13:06 -0500

    consort config

--- pi.sh
+++ pi.sh
@@ -94,17 +94,12 @@ sudo systemctl enable --now mpd.service
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
 
-echo '
+if [ "$wifi_mode" = "create" ]; then
+    echo '
 interface wlanboard
    static ip_address=192.168.5.1/24
    nohook wpa_supplicant
 ' | sudo tee -a /etc/dhcpcd.conf
-
-echo 'net.ipv4.ip_forward=1' |sudo tee -a /etc/sysctl.d/routed-ap.conf
-sudo iptables -t nat -A POSTROUTING -o $wan_interface -j MASQUERADE
-sudo netfilter-persistent save
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-
 echo '
 no-hosts
 interface=wlanboard
@@ -113,8 +108,31 @@ dhcp-range=192.168.5.100,192.168.5.200,255.255.255.0,24h
 domain=wlan     # Local wireless DNS domain
 address=/gw.wlan/192.168.5.1  # Alias for this router
 '| sudo tee /etc/dnsmasq.conf
-
 echo "192.168.5.1 $host_name.local" | sudo tee -a /etc/hosts
+fi
+
+
+echo 'net.ipv4.ip_forward=1' |sudo tee -a /etc/sysctl.d/routed-ap.conf
+sudo iptables -t nat -A POSTROUTING -o $wan_interface -j MASQUERADE
+sudo netfilter-persistent save
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+
+if [ "$eth0_mode" = "consort" ]; then
+        echo '
+interface eth0
+   static ip_address=10.0.4.2
+' | sudo tee -a /etc/dhcpcd.conf
+echo '
+no-hosts
+interface=eth0
+dhcp-range=10.0.4.100,10.0.4.200,255.255.255.0,24h
+                # Pool of IP addresses served via DHCP
+domain=local     # Local wireless DNS domain
+address=/gw.local/10.0.4.2  # Alias for this router
+dhcp-host=14:7d:da:14:83:9f,10.0.4.200
+' | sudo tee /etc/dnsmasq.conf
+echo "10.0.4.2 $host_name.local" | sudo tee -a /etc/hosts
+fi
 
 #git clone https://github.com/lwfinger/rtl8188eu.git
 #cd rtl8188eu
