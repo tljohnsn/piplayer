9216dfee6b702344f776db4e15a3dfb87ff4fd67
Your Name <you@example.com>
2023-02-15 09:43:45 -0500

    Depth plot

---
+++ scripts/depth.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use DateTime;
+use IO::Socket::INET;
+use List::Util qw(sum);
+
+
+my $socket = new IO::Socket::INET(
+    PeerHost => '10.0.4.182',
+    PeerPort => '10110',
+    Protocol  => 'tcp'
+) or die "Socket konnte nicht erstellt werden!\n$!\n";
+
+print "Client kommuniziert auf Port 5005\n";
+my $dt = DateTime->now;
+$dt->set_time_zone('America/New_York');
+my $prevfive = int($dt->minute/5);
+my @depths = ();
+while (my $line = $socket->getline) {
+    if ($line=~ /DBT/){
+	$dt = DateTime->now;
+	$dt->set_time_zone('America/New_York');
+	my $five = int($dt->minute/5);
+	my @data=split(",",$line);
+	my $datestring=$dt->datetime();
+	$datestring = $dt->ymd('-') . ' ' . $dt->hms(':');
+	print "$datestring $five Depth is $data[1]\n"; # do whatever with your data here
+	if ($prevfive != $five) {
+	    my $average = sum(@depths)/@depths;
+	    my $filename = "//home/pi/data";
+	    open(FH, '>>', $filename) or die $!;
+	    print "$datestring $five averave Depth is $average\n";
+	    my $data = "$datestring $average\n";
+	    print FH $data;
+	    close(FH);
+	    $prevfive = $five;
+	    @depths =();
+	} else {
+	    push (@depths, $data[1]);
+	}
+    }
+}
---
+++ scripts/plot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#https://ben.akrin.com/gnuplot-one-liner-from-hell/
+#export width=`stty size | cut -d " " -f2`; export height=`stty size | cut -d " " -f1`-10; cat /tmp/data | sed "s/ /T/" | gnuplot -e "set terminal dumb $width $height; set autoscale; set xdata time; set timefmt \"%Y-%m-%dT%H:%M:%S\"; set xlabel \"time\"; set ylabel \"depth\"; plot '-' using 1:2 with lines"
+export width=`stty size | cut -d " " -f2`; export height=`stty size | cut -d " " -f1`-10; cat /home/pi/data | sed "s/ /T/" | gnuplot -e "set terminal png ; set autoscale; set xdata time; set timefmt \"%Y-%m-%dT%H:%M:%S\"; set xlabel \"time\"; set ylabel \"depth\"; plot '-' using 1:2 with lines" | ./.local/bin/imgcat
