ea648cb5717523e3bba3c611126237f377bfe9c5
tljohnsn <tljohnsn@macbook2020.mhs>
2023-04-21 09:41:05 -0400

    Fix SHM: difference limit exceeded

--- scripts/install-gpsd.sh
+++ scripts/install-gpsd.sh
@@ -7,6 +7,16 @@ systemctl stop gpsd.service
 systemctl disable gpsd.socket
 systemctl disable gpsd.service
 
+systemctl mask gpsd.socket
+systemctl mask gpsd.service
+
+if [ `grep -c 127.127.28.0 /etc/ntp.conf` -lt 1 ]; then
+echo "
+server 127.127.28.0 prefer
+fudge 127.127.28.0 flag1 1 refid PPS
+" | sudo tee -a /etc/ntp.conf
+fi
+
 #test with
 #gpsd /dev/ttyACM0 -G -n -N
 
