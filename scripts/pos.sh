52631b1cf13532942f579ffcb1c5bcf57dbf999c
Your Name <you@example.com>
2023-08-28 07:52:45 -0400

    grid

--- scripts/pos.sh
+++ scripts/pos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 x=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f9|cut -d"," -f1)
 y=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f10|cut -d"," -f1)
-echo "$x,$y"
+echo "$x $y"
+echo $y $x | ./lonlat2maiden 6
