1fb7500197c77f820ed917cd0be2669efc03751c
Your Name <you@example.com>
2023-11-04 09:56:45 -0400

    Replace the config

--- scripts/pos.sh
+++ scripts/pos.sh
@@ -5,5 +5,5 @@ x=$(gpspipe -w -n 10 |grep lon|tail -n1 | awk -F lon '{print $2}' | cut -d : -f 
 y=$(gpspipe -w -n 10 |grep lat|tail -n1 | awk -F lat '{print $2}' | cut -d : -f 2 | cut -d , -f1)
 echo "$x $y"
 echo $x $y | ./lonlat2maiden 6
-sed -i -e 's%locator":.*,%locator": "EL97VE",%' ~/.config/pat/config.json
+sed -i -e "s%locator\":.*,%locator\": \"`echo $x $y | ./lonlat2maiden 6`\",%" ~/.config/pat/config.json
 
