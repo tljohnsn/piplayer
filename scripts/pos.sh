ba468a644b92f83a1294f0d3e8e5fa480d274bca
Your Name <you@example.com>
2023-11-04 09:38:56 -0400

    Fix position

--- scripts/pos.sh
+++ scripts/pos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-x=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f9|cut -d"," -f1)
+x=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f11|cut -d"," -f1)
 y=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f10|cut -d"," -f1)
 echo "$x $y"
-echo $y $x | ./lonlat2maiden 6
+echo $x $y | ./lonlat2maiden 6
