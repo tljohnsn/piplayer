9d9b737d29c6aa1a65ce2707f2e7d37697c54697
Your Name <you@example.com>
2023-11-04 09:52:56 -0400

    awk instead of cut

--- scripts/pos.sh
+++ scripts/pos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-x=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f11|cut -d"," -f1)
-y=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f10|cut -d"," -f1)
+#x=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f11|cut -d"," -f1)
+x=$(gpspipe -w -n 10 |grep lon|tail -n1 | awk -F lon '{print $2}' | cut -d : -f 2 | cut -d , -f1)
+#y=$(gpspipe -w -n 10 |grep lat|tail -n1|cut -d":" -f10|cut -d"," -f1)
+y=$(gpspipe -w -n 10 |grep lat|tail -n1 | awk -F lat '{print $2}' | cut -d : -f 2 | cut -d , -f1)
 echo "$x $y"
 echo $x $y | ./lonlat2maiden 6
+sed -i -e 's%locator":.*,%locator": "EL97VE",%' ~/.config/pat/config.json
+
