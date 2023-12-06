26801840dc0415ac36655024ea79f74d68179afb
Your Name <you@example.com>
2023-12-06 07:14:05 -0500

    sound

---
+++ scripts/startsound.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sleep 10
+su -c "aplay /usr/share/sounds/alsa/Front_Center.wav" - pi | tee -a /var/log/aplay.log &
+su -c "aplay -t raw -r 48000 -c 2 -f S16_LE /dev/zero" - pi | tee -a /var/log/aplay.log &
