6ebf6a6e4b90331a71a87804358610d78beb84e3
Your Name <you@example.com>
2023-12-07 12:40:00 -0500

    rtorrent

--- scripts/install-rtorrent.sh
+++ scripts/install-rtorrent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/pi
 sudo apt-get -y install screen rtorrent
-cp -a /home/pi/piplayer/configconfigfiles/rtorrent.rc /home/pi/.rtorrent.rc
+cp -a /home/pi/piplayer/configfiles/rtorrent.rc /home/pi/.rtorrent.rc
 
 mkdir ~/rtorrent
