5a406d0e4d633506b25129e72c48b4917be6031d
Your Name <you@example.com>
2023-12-08 07:17:29 -0600

    boot

--- scripts/btboot.sh
+++ scripts/btboot.sh
@@ -3,3 +3,7 @@ sleep 10
 device=`bluetoothctl devices | cut -d " " -f 2`
 bluetoothctl connect $device
 aplay /usr/share/sounds/alsa/Front_Left.wav
+mpc enable 1
+mpc crossfade 10
+mpc consume on
+mpc volume 80%
