0584abcbcee90d4b0fb00d59642c7a828ebe8a11
Your Name <you@example.com>
2023-12-11 15:21:26 -0600

    gps

--- scripts/install-gpsd.sh
+++ scripts/install-gpsd.sh
@@ -17,6 +17,13 @@ fudge 127.127.28.0 flag1 1 refid PPS
 " | sudo tee -a /etc/ntp.conf
 fi
 
+if [ `grep -c 127.127.28.0 /etc/ntpsec/ntp.conf` -lt 1 ]; then
+echo "
+server 127.127.28.0 prefer
+fudge 127.127.28.0 flag1 1 refid PPS
+" | sudo tee -a /etc/ntpsec/ntp.conf
+fi
+
 #test with
 #gpsd /dev/ttyACM0 -G -n -N
 
