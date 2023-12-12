17c335b60b28ffd09595bd27f9c1c653c9e3b15d
Your Name <you@example.com>
2023-12-12 11:58:22 -0600

    bookworm

--- scripts/startsound.sh
+++ scripts/startsound.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-su -c "mpd /home/pi/mpd.conf" - pi | tee -a /var/log/aplay.log
+source /etc/os-release
+if [ "$VERSION_CODENAME" = "bookworm" ]; then
+   su -c "mpd /home/pi/piplayer/configfiles/mpd.bookworm.conf" - pi | tee -a /var/log/aplay.log
+fi
 sleep 10
 su -c "aplay /usr/share/sounds/alsa/Front_Center.wav" - pi | tee -a /var/log/aplay.log &
 su -c "aplay -t raw -r 48000 -c 2 -f S16_LE /dev/zero" - pi | tee -a /var/log/aplay.log &
