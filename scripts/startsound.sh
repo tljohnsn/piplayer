477509a47fb12e3a67bf588ecc36063cafec16bb
tljohnsn <tljohnsn@macbook2020.local>
2023-12-12 15:29:01 -0500

    a

--- scripts/startsound.sh
+++ scripts/startsound.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /etc/os-release
-if [ "$VERSION_CODENAME" = "bookworm" ]; then
-   su -c "mpd /home/pi/piplayer/configfiles/mpd.bookworm.conf" - pi | tee -a /var/log/aplay.log
-fi
 sleep 10
 su -c "aplay /usr/share/sounds/alsa/Front_Center.wav" - pi | tee -a /var/log/aplay.log &
 su -c "aplay -t raw -r 48000 -c 2 -f S16_LE /dev/zero" - pi | tee -a /var/log/aplay.log &
+if [ "$VERSION_CODENAME" = "bookworm" ]; then
+   su -c "mpd /home/pi/piplayer/configfiles/mpd.bookworm.conf" - pi | tee -a /var/log/aplay.log
+fi
