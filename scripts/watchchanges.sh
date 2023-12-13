1546eecb3b81029ad54de16072072d13fa0607de
tljohnsn <tljohnsn@bankbook1.local>
2023-12-13 06:57:59 -0500

    watch

--- scripts/watchchanges.sh
+++ scripts/watchchanges.sh
@@ -3,6 +3,8 @@ NOW=`date +'%s'`
 LASTRUN=`date +'%s'`
 PAST=`date +'%s'`
 echo starting $NOW
+LOCK="/tmp/watch.lock"
+rm -f $LOCK
 while [ 1 == 1 ]; do
     echo "Starting idle loop"
     while mpc idle database; do
@@ -10,7 +12,14 @@ while [ 1 == 1 ]; do
 	RUNDURATION=`expr $NOW - $LASTRUN`
 	CHANGEDURATION=`expr $NOW - $PAST`
 	echo it changed NOW: $NOW LASTRUN: $LASTRUN RUNDURATION: $RUNDURATION PAST: $PAST CHANGEDURATION: $CHANGEDURATION
-	~pi/piplayer/scripts/update-rompr.sh
+	if [ ! -f "$LOCK" ]; then
+	    touch $LOCK
+	    echo "it was unlocked"
+	    ~pi/piplayer/scripts/update-rompr.sh
+	    rm -f $LOCK
+	else
+	    echo "it was locked"
+	fi
 #    if [[ ( $RUNDURATION -gt 10 ) && ( $CHANGEDURATION -gt 10 ) ]]; then
 #	echo Rebuild the index
 #	sleep 11
