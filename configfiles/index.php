f191a802791eba11788e9bb10ce07c482f702173
tljohnsn <tljohnsn@macbook2020.local>
2023-12-14 06:36:28 -0500

    a

--- configfiles/index.php
+++ configfiles/index.php
@@ -3,6 +3,7 @@ echo '<font size=7><br>';
 echo '<a href=http://' .$_SERVER["HTTP_HOST"] . ':6680/iris/library/artists/?ui={"initial_setup_complete":true}>Iris</a><br><br>';
 echo '<a href=http://' .$_SERVER["HTTP_HOST"] . '/ampache/#browse.php?action=artist>Ampache</a><br><br>';
 echo '<a href=http://' .$_SERVER["HTTP_HOST"] . '/rompr/>RompR</a><br><br>';
+echo '<a href=http://' .$_SERVER["HTTP_HOST"] . '/rompr/albumart.php>RompR art</a><br><br>';
 echo '<a href=http://' .$_SERVER["HTTP_HOST"] . '/rompr/?setup>Setup</a><br><br>';
 echo '<a href=http://' .$_SERVER["HTTP_HOST"] . '/rompr/albumart.php>Artwork</a><br><br>';
 echo '</font>';
