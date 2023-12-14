a5960a9d661d5df74a5cf0d18d2a3dd975b30fbb
tljohnsn <tljohnsn@macbook2020.local>
2023-12-14 10:12:24 -0500

    a

--- scripts/startsound.sh
+++ scripts/startsound.sh
@@ -6,4 +6,5 @@ su -c "aplay -t raw -r 48000 -c 2 -f S16_LE /dev/zero" - pi | tee -a /var/log/ap
 if [ "$VERSION_CODENAME" = "bookworm" ]; then
    su -c "mpd /home/pi/piplayer/configfiles/mpd.bookworm.conf" - pi | tee -a /var/log/aplay.log
 fi
-amixer sset 'Master' 80 %
+amixer sset 'Master' 50%
+
