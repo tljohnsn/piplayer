13ff0d671adca945eb3e397a530fa11967a760c4
tljohnsn <tljohnsn@macbook2020.local>
2024-03-30 12:03:20 -0400

    tide

--- tagger/go.sh
+++ tagger/go.sh
@@ -47,4 +47,5 @@ echo "If everything looks good, drag the folders from mp3zrenamed into mp3zlapto
 echo 
 echo "now run this command to send files from mp3zlaptop to tunes:"
 echo " ~/piplayer/tagger/syncssh.sh"
-echo
+echo " mpc update"
+echo " mpc add mp3zpi/Square\ One\ -\ In\ Full\ Bloom/10\ Kitty\ Cat.mp3"
