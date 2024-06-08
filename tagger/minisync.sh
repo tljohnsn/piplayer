675030bee442ae16f50a16aa36b9e75dba0d0e98
Trent Johnson <tljohnsn@MacMini.local>
2024-06-08 06:30:50 -0500

    mini

---
+++ tagger/minisync.sh
@@ -0,0 +1,2 @@
+rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/. pi@tunes.local:rtorrent/download/.
+rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/adtorrent2/. pi@tunes.local:rtorrent/watch/start/.
