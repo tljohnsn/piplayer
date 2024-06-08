93ffa43d94b7dfbc8cd524c41b87b71cf9cc2f3e
tljohnsn <tljohnsn@macbook2020.local>
2024-06-08 06:32:11 -0500

    encode

--- tagger/encode.sh
+++ tagger/encode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#ffmpeg -i "$1" -f wav - | lame -V 0 --noreplaygain - "$2"
-ffmpeg -i "$1" -f wav - | lame -b 320 --noreplaygain - "$2"
+ffmpeg -i "$1" -f wav - | lame -V 0 --noreplaygain - "$2"
+#ffmpeg -i "$1" -f wav - | lame -b 320 --noreplaygain - "$2"
