70e80c3e2ab098554103d3d607ca258e1cc33ee5
Trent Johnsn <tljohnsn@xw4600.(none)>
2024-07-07 14:06:23 -0500

    mini

--- tagger/minisync.sh
+++ tagger/minisync.sh
@@ -1,2 +1,5 @@
 rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/. pi@tunes.local:rtorrent/download/.
 rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/adtorrent2/. pi@tunes.local:rtorrent/watch/load/.
+
+rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/. trentj@seed.useractive.com:rtorrent/download/.
+rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/adtorrent2/. trentj@seed.useractive.com:rtorrent/watch/load/.
