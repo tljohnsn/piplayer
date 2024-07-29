9db7fbd0a7dd8d2cde70dcee503cfae3a8d840ba
tljohnsn <tljohnsn@macbook2020.local>
2024-07-29 17:20:23 -0500

    minisync

--- tagger/minisync.sh
+++ tagger/minisync.sh
@@ -1,5 +1,8 @@
-rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/. pi@tunes.local:rtorrent/download/.
-rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/adtorrent2/. pi@tunes.local:rtorrent/watch/load/.
+ssh -A macmini.local rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/. pi@tunes.local:rtorrent/download/.
+ssh -A macmini.local rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/adtorrent2/. pi@tunes.local:rtorrent/watch/load/.
 
-rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/. trentj@server17.useractive.com:rtorrent/download/.
-rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/adtorrent2/. trentj@server17.useractive.com:rtorrent/watch/load/.
+ssh -A macmini.local rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/. trentj@server17.useractive.com:rtorrent/download/.
+ssh -A macmini.local rsync -avP -e ~/bin/nssh /Users/tljohnsn/Movies/Torrents/adtorrent2/. trentj@server17.useractive.com:rtorrent/watch/load/.
+
+ssh -A trentj@server17.useractive.com rsync -avP rtorrent/download/. server18:rtorrent/download/.
+ssh -A trentj@server18.useractive.com cp rtorrent/download/adtorrent2/*.torrent rtorrent/watch/load/
