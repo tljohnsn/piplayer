6f8ca961c51ee1030c8eb1b7cb96701a9ca4a006
Trent Johnson <tljohnsn@bankbook0.local>
2025-06-05 11:22:29 -0500

    v0

--- tagger/encode.sh
+++ tagger/encode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#ffmpeg -i "$1" -f wav - | lame -V 0 --noreplaygain - "$2"
-ffmpeg -i "$1" -f wav - | lame -b 320 --noreplaygain - "$2"
+ffmpeg -i "$1" -f wav - | lame -V 0 --noreplaygain - "$2"
+#ffmpeg -i "$1" -f wav - | lame -b 320 --noreplaygain - "$2"
 #echo ffmpeg -i \""$1"\" -acodec alac "$2" >>/tmp/convert.out
 #ffmpeg -i "$1" -f wav - | ffmpeg -acodec alac "$2"
