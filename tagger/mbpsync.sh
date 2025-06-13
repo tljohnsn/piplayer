96c1e09e09e5ded5aca04d47fdc3bc3f1e1bae55
tljohnsn <tljohnsn@bankbook1.local>
2025-06-13 06:07:00 -0500

    a

--- tagger/mbpsync.sh
+++ tagger/mbpsync.sh
@@ -3,7 +3,7 @@
 #rsync -avP --del -e ~/bin/nssh trentj@server17.useractive.com:rtorrent/watch/load/. ~/red/adtorrent2/.
 #xrsync -avP --del -e ~/bin/nssh trentj@server17.useractive.com:rtorrent/download/. ~/red/download/.
 
-/usr/local/bin/rsync -avP --iconv=utf-8-mac,utf-8 ~/red/download/. trentj@server17.useractive.com:rtorrent/download/.
+/usr/local/bin/rsync -avP --iconv=utf-8-mac,utf-8 --exclude=.sync --exclude=.DS_Store ~/red/download/. trentj@server17.useractive.com:rtorrent/download/.
 rsync -avP ~/red/adtorrent2/. trentj@server17.useractive.com:rtorrent/watch/load/.
 
 ssh -A trentj@server17.useractive.com rsync -avP rtorrent/download/. server18:rtorrent/download/.
