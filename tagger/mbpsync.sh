0881804b07af17bd30af8c2103445357b2a7ad90
tljohnsn <tljohnsn@macbook2020.local>
2025-06-20 06:26:41 -0500

    a

--- tagger/mbpsync.sh
+++ tagger/mbpsync.sh
@@ -4,7 +4,7 @@
 #xrsync -avP --del -e ~/bin/nssh trentj@server17.useractive.com:rtorrent/download/. ~/red/download/.
 
 /usr/local/bin/rsync -avP --iconv=utf-8-mac,utf-8 --exclude=.sync --exclude=.DS_Store ~/red/download/. trentj@server17.useractive.com:rtorrent/download/.
-rsync -avP ~/red/adtorrent2/. trentj@server17.useractive.com:rtorrent/watch/load/.
+rsync -avP --exclude=.DS_Store ~/red/adtorrent2/. trentj@server17.useractive.com:rtorrent/watch/load/.
 
 ssh -A trentj@server17.useractive.com rsync -avP rtorrent/download/. server18:rtorrent/download/.
 rsync -avP ~/red/adtorrent2/. trentj@server18.useractive.com:rtorrent/watch/load/.
