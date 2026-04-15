096c683fcec164632f09b661bf4e11c25b164159
tljohnsn <tljohnsn@macbook2020.local>
2026-04-15 14:05:16 -0500

    torrent

--- installer/build-installer.sh
+++ installer/build-installer.sh
@@ -8,7 +8,8 @@ ln -s ~/piplayer/installer/custom.postinst .
 
 
 sudo xorriso -osirrox on -indev debian-12-i386-CD-1.iso  -extract / isofiles/
-sudo chmod u+w ./simple-cdd/custom.postinst
+sudo chmod u+w isofiles/simple-cdd/custom.postinst
+sudo cp ~/piplayer/installer/custom.postinst isofiles/simple-cdd/custom.postinst
 cd isofiles/simple-cdd/
 sudo tar -xzf extras.tar.gz
 #do stuff in extras dir
@@ -21,4 +22,9 @@ sudo chmod u-w ./simple-cdd/custom.postinst
 sudo chmod a+w md5sum.txt
 sudo md5sum `gfind -follow -type f` > md5sum.txt
 sudo chmod a-w md5sum.txt
+cd ..
+sudo genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o debian-12-unattended.iso isofiles
 
+rsync -avP -e "nssh -p 2222" pi@localhost:debian-12-unattended.iso .
+nssh -p 2222 pi@localhost
+nssh -p 2223 pi@localhost
--- scripts/install-rtorrent.sh
+++ scripts/install-rtorrent.sh
@@ -11,8 +11,30 @@ chmod 755 ~/.
 
 wget https://github.com/Novik/ruTorrent/archive/refs/tags/v4.3.2.tar.gz
 tar -xzf v4.3.2.tar.gz
+
+#lastactive plugin created from seedingtime
+cp -a ruTorrent-4.3.2/plugins/seedingtime ruTorrent-4.3.2/plugins/lastactive
+cd ruTorrent-4.3.2/plugins/lastactive
+sed -i -e "s/Finished/Activity/" lang/*.js
+sed -i -e "s/seedingTime/lastActivity/g" init.js lang/*.js
+sed -i -e "s/seedingtime/lastactive/g" init.js done.php init.php
+sed -i -e "s/SeedingTime/LastActive/g" init.js lang/*.js
+sed -i -e 's/d.get_custom=")+"lastactive"/d.last_active=")/' init.js
+sed -i -e 's/"addtime"/"addtime1"/g' init.js
+sed -i -e "s/torrent.addtime /torrent.addtime1 /" init.js
+sed -i -e "9d" init.js
+cd ../../..
+
 sudo chown -R www-data.www-data ruTorrent-4.3.2
+rm -rf ruTorrent-4.3.2/plugins/{_cloudflare,screenshots,mediainfo,spectrogram}
+
 sudo mv ruTorrent-4.3.2 /var/www/html
-sudo ln -s /var/www/html/ruTorrent-4.3.2 /var/www/html/rut
+sudo ln -s /var/www/html/ruTorrent-4.3.2 /var/www/html/ru
+
+sudo install -b -o www-data -g www-data -m 755 ~pi/piplayer/configfiles/WebUISettings.dat /var/www/html/rut/share/settings/WebUISettings.dat
 
 sudo sed -i -e "s/5000/5001/" /var/www/html/rut/conf/config.php
+
+cd ~pi
+git clone https://github.com/tljohnsn/rtorrent_orphan_cleanup.git
+install -b -u pi -g pi -m 600 ~pi/piplayer/configfiles/orphan_cleanup.json ~pi/rtorrent_orphan_cleanup/orphan_cleanup.json
