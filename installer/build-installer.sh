4d0132ee54cd3143948d43fce4017cf06beab834
Trent Johnson <tljohnsn@bankbook0.local>
2026-04-27 16:02:42 -0500

    a

--- installer/build-installer.sh
+++ installer/build-installer.sh
@@ -10,7 +10,7 @@ cd ..
 sed -i -e s/ftp.de.debian.org/ftp.us.debian.org/ custom.conf
 tar -pczf extras.tar.gz extras/
 build-simple-cdd --conf custom.conf --auto-profiles custom --locale en_US --keyboard us --force-root --force-preseed --no-do-mirror --dvd --logfile ./out.log
-
+dd if=proxmox-ve_9.1-1-auto-from-iso.iso of=/dev/sda bs=4M status=progress
 
 proxmox-auto-install-assistant prepare-iso proxmox-ve_9.1-1.iso --fetch-from iso --answer-file answer.toml --on-first-boot firstboot.sh
 
--- installer/firstboot.sh
+++ installer/firstboot.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-apt -y install ifupdown2 emacs-nox sudo rsyslog libnss-mdns \
-    proxmox-auto-install-assistant xorriso simple-cdd build-essential net-tools
 touch /root/trent.was.here
 sed -i -e "s/bridge-fd 0/bridge-fd 0\n\tbridge-vlan-aware yes\n\tbridge-vids 2-4094/" /etc/network/interfaces
 echo "thunderbolt" | tee -a /etc/modules
@@ -13,6 +11,15 @@ Components: pve-no-subscription
 Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
 " | tee /etc/apt/sources.list.d/proxmox.sources
 
+echo "KexAlgorithms +diffie-hellman-group1-sha1
+PubkeyAcceptedAlgorithms=+ssh-rsa
+HostKeyAlgorithms=+ssh-rsa
+" | tee -a /etc/ssh/sshd_config
+
+echo "HostKeyAlgorithms=+ssh-rsa
+PubkeyAcceptedAlgorithms=+ssh-rsa
+" | tee -a /etc/ssh/ssh_config
+
 echo "Enabled: no" | tee -a /etc/apt/sources.list.d/pve-enterprise.sources
 
 sed -i -e "s/enterprise.proxmox/download.proxmox/" /etc/apt/sources.list.d/ceph.sources
@@ -20,3 +27,39 @@ sed -i -e "s/enterprise/no-subscription/"  /etc/apt/sources.list.d/ceph.sources
 
 
 echo "set enable-bracketed-paste off" | tee -a /etc/inputrc
+
+apt -y update
+apt -y install ifupdown2 emacs-nox sudo rsyslog libnss-mdns \
+    proxmox-auto-install-assistant xorriso simple-cdd build-essential net-tools
+
+git clone https://github.com/tljohnsn/piplayer.git /home/pi/piplayer
+
+cat ~/piplayer/configfiles/bashrc.txt >>~root/.bashrc
+cat ~/piplayer/configfiles/bashrc.txt >>/etc/skel/.bashrc
+
+useradd -m -s /bin/bash -u 1025 -p "$y$j9T$T4hKMWt/iUBHQ15MpKjG31$MgIwrN16i2tleH1GLg4lwh6e3LeuIlMo2C9rc8gcPnD" tljohnsn
+mkdir -p /home/tljohnsn/.ssh
+cp /root/.ssh/authorized_keys /home/tljohnsn/.ssh
+chown tljohnsn /home/tljohnsn/.ssh/authorized_keys
+
+echo "tljohnsn ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers.d/010_tljohnsn-nopasswd
+chmod 440 /etc/sudoers.d/010_tljohnsn-nopasswd
+systemctl enable --now avahi-daemon
+systemctl disable rsync
+
+echo "" | tee /etc/motd
+
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+echo "[Service]" | tee /etc/systemd/system/getty@tty1.service.d/noclear.conf
+echo "TTYVTDisallocate=no" | tee -a /etc/systemd/system/getty@tty1.service.d/noclear.conf
+echo ExecStart= | tee -a /etc/systemd/system/getty@tty1.service.d/noclear.conf
+echo ExecStart=-/sbin/agetty --noclear %I $TERM | tee -a /etc/systemd/system/getty@tty1.service.d/noclear.conf
+
+lvcreate -n images -L200G pve
+mkfs.ext4 -L images  /dev/pve/images
+mkdir /images
+echo "/dev/pve/images /images ext4 defaults 1 2" | tee -a /etc/fstab
+mount -a
+pvesm add dir images --path /images --content images,iso,backup --is_mountpoint 1
+
+
