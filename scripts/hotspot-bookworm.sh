38659b24cc048615c31cfdf4bfdba20ea39f4814
tljohnsn <tljohnsn@macbook2026.local>
2026-07-06 11:06:58 -0500

    a

--- scripts/hotspot-bookworm.sh
+++ scripts/hotspot-bookworm.sh
@@ -27,24 +27,27 @@ source /boot/firmware/tunes.txt
 #nmcli con modify TEST-AP wifi-sec.key-mgmt wpa-psk
 #nmcli con modify TEST-AP wifi-sec.psk "9e6a188321"
 
-#sudo nmcli device wifi hotspot ifname wlan0 ssid pi password 9e6a188321
 #sudo nmcli con mod Hotspot  autoconnect true
 
-#sudo nmcli connection add type wifi con-name ArubaPhone11 ifname wlan1 ssid "ArubaPhone11" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "9e6a188321"
-#sudo nmcli device wifi rescan ifname wlan1
-#sudo nmcli device wifi list   ifname wlan1
-#sudo nmcli con mod preconfigured connection.interface-name ArubaPhone11
+sudo nmcli connection add type wifi con-name ArubaPhone11 ifname wlan3 ssid "$join_wifi_network" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "join_wifi_password"
+sudo nmcli device wifi rescan ifname wlan3
+sudo nmcli device wifi list   ifname wlan3
 
 sudo nmcli device wifi hotspot ifname wlanboard con-name "$create_wifi_network" ssid "$create_wifi_network"  password "$create_wifi_password"
 sudo nmcli con mod "$create_wifi_network" autoconnect true
-nmcli dev wifi show-password
 
 
 if [ ! -z "$join_wifi_password" ]; then
-    sudo nmcli device wifi connect $join_wifi_network password "$join_wifi_password" ifname wlan1
+    #sudo nmcli device wifi connect $join_wifi_network password "$join_wifi_password" ifname wlan1
 else
-    sudo nmcli device wifi connect $join_wifi_network --ask ifname wlan1
+    #sudo nmcli device wifi connect $join_wifi_network --ask ifname wlan1
 fi
 
 sudo nmcli con mod preconfigured connection.interface-name wlan1
+sudo nmcli con mod "$create_wifi_network" connection.interface-name wlanboard
+sudo nmcli con mod "$join_wifi_network" connection.interface-name wlan3
+sudo nmcli con mod "$create_wifi_network" autoconnect true
+sudo nmcli con mod "$join_wifi_network" autoconnect true
+sudo nmcli con mod preconfigured autoconnect true
 
+nmcli dev wifi show-password
