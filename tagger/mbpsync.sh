a54c970d1dbdd5556a91e243cb5eb0d23a6e4e33
tljohnsn <tljohnsn@macbook2026.local>
2026-07-31 06:04:16 -0500

    a

--- tagger/mbpsync.sh
+++ tagger/mbpsync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #mkdir -p ~/red/{download,adtorrent2}
-#rsync -avP --del -e ~/bin/nssh trentj@server17.useractive.com:rtorrent/watch/load/. ~/red/adtorrent2/.
-#xrsync -avP --del -e ~/bin/nssh trentj@server17.useractive.com:rtorrent/download/. ~/red/download/.
+#rsync -avP --del -e ~/bin/nssh trentj@server18.trentjohnson.net:rtorrent/watch/load/. ~/red/adtorrent2/.
+#xrsync -avP --del -e ~/bin/nssh trentj@server18.trentjohnson.net:rtorrent/download/. ~/red/download/.
 
-/usr/local/bin/rsync -avP --iconv=utf-8-mac,utf-8 --exclude=.sync --exclude=.DS_Store ~/red/download/. trentj@server17.useractive.com:rtorrent/download/.
-rsync -avP --exclude=.DS_Store ~/red/adtorrent2/. trentj@server17.useractive.com:rtorrent/watch/load/.
+/usr/local/bin/rsync -avP --iconv=utf-8-mac,utf-8 --exclude=.sync --exclude=.DS_Store ~/red/download/. trentj@server18.trentjohnson.net:rtorrent/download/.
+rsync -avP --exclude=.DS_Store ~/red/adtorrent2/. trentj@server18.trentjohnson.net:rtorrent/watch/load/.
 
-ssh -A trentj@server17.useractive.com rsync -avP rtorrent/download/. server18:rtorrent/download/.
-rsync -avP ~/red/adtorrent2/. trentj@server18.useractive.com:rtorrent/watch/load/.
+ssh -A trentj@server18.trentjohnson.net rsync -avP rtorrent/download/. server25.trentjohnson.net:rtorrent/download/.
+rsync -avP ~/red/adtorrent2/. trentj@server25.trentjohnson.net:rtorrent/watch/load/.
 
