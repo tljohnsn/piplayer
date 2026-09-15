4341a2e92283d42e40f2e2d2f65ae54a22f47466
tljohnsn <tljohnsn@macbook2026.lan>
2026-09-15 06:58:52 -0500

    a

--- installer/postinstall.sh
+++ installer/postinstall.sh
@@ -45,32 +45,3 @@ sudo echo "deb http://download.proxmox.com/debian/pbs trixie pbs-no-subscription
     | sudo tee -a /etc/apt/sources.list.d/proxmox-backup-server.list
 echo "deb [signed-by=/etc/apt/keyrings/hp-mcp.gpg] https://downloads.linux.hpe.com/SDR/repo/mcp/debian trixie/current non-free" |sudo tee -a /etc/apt/sources.list.d/hp-mcp.list
 
-
-apt update
-apt -y install ssacli
-
-mkdir -p /etc/proxmox-backup/certs.bak
-cp /etc/proxmox-backup/proxy.pem /etc/proxmox-backup/certs.bak
-cp /etc/proxmox-backup/proxy.key /etc/proxmox-backup/certs.bak
-#cp /root/piplayer/configfiles/wildcards.crt /etc/proxmox-backup/proxy.pem
-#cat ~/uwca/wildcards.key
-#cat >/etc/proxmox-backup/proxy.key
-#systemctl reload proxmox-backup-proxy
-
-dd if=/dev/zero of=/safemount.img bs=4M count=1
-mkfs.ext4 -L safemount /safemount.img
-mkdir /safemount
-mount -o loop /safemount.img /safemount
-mkdir /safemount/aspace
-sudo mdadm --create --verbose /dev/md0 --level=5 --raid-devices=4 /dev/sdb1 /dev/sdc1 /dev/sdd1 /dev/sde1
-mkfs.ext4 -L aspace /dev/md0
-mount -L aspace /safemount/aspace/
-
-
-mkfs.ext4 -L space /dev/sda1
-mkdir /space
-mount -L space /space
-
-#ssacli ctrl slot=1 modify dwc=enable forced
-#ssacli ctrl slot=1 modify nbwc=enable
-
