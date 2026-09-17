bde0f0da328748e2259816b876cd0202c49462f7
tljohnsn <tljohnsn@macbook2026.lan>
2026-09-17 06:04:09 -0500

    a

--- installer/pbs.sh
+++ installer/pbs.sh
@@ -1,39 +1,6 @@
-#!/bin/bash
-#apt -y install libnss-mdns
-echo "set enable-bracketed-paste off" | tee -a /etc/inputrc
-
-echo "KexAlgorithms +diffie-hellman-group1-sha1
-PubkeyAcceptedAlgorithms=+ssh-rsa
-HostKeyAlgorithms=+ssh-rsa
-" | tee -a /etc/ssh/sshd_config
-
-echo "HostKeyAlgorithms=+ssh-rsa
-PubkeyAcceptedAlgorithms=+ssh-rsa
-" | tee -a /etc/ssh/ssh_config
 
 #apt -y install emacs-nox sudo rsyslog libnss-mdns git pv gpg curl
-#apt -y install fupdown2 rdnssd
-#git clone https://github.com/tljohnsn/piplayer.git /root/piplayer
-cat /root/piplayer/configfiles/bashrc.txt >>~root/.bashrc
-cat /root/piplayer/configfiles/bashrc.txt >>/etc/skel/.bashrc
-cat /root/piplayer/configfiles/bashrc.txt >>/home/tljohnsn/.bashrc
-
-#setup keys
-mkdir -p /home/tljohnsn/.ssh
-sshkey0=`grep ^sshkey0 /root/piplayer/pi.sh | cut -c 9-`
-echo "ssh-rsa $sshkey0 newlaptopkey" >>~tljohnsn/.ssh/authorized_keys
-echo "ssh-rsa $sshkey0 newlaptopkey" >>~root/.ssh/authorized_keys
-
-chown tljohnsn:tljohnsn ~tljohnsn/.ssh/authorized_keys /home/tljohnsn/.ssh
-chown root:root ~root/.ssh/authorized_keys
-
-chmod 600  ~tljohnsn/.ssh/authorized_keys ~root/.ssh/authorized_keys
-
-echo "tljohnsn ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers.d/010_tljohnsn-nopasswd
-chmod 440 /etc/sudoers.d/010_tljohnsn-nopasswd
-systemctl enable --now avahi-daemon
-systemctl disable rsync
-
+apt -y install rdnssd
 #Install repos
 sudo wget https://enterprise.proxmox.com/debian/proxmox-release-trixie.gpg \
      -O /etc/apt/trusted.gpg.d/proxmox-release-trixie.gpg
@@ -52,10 +19,10 @@ apt -y install ssacli
 mkdir -p /etc/proxmox-backup/certs.bak
 cp /etc/proxmox-backup/proxy.pem /etc/proxmox-backup/certs.bak
 cp /etc/proxmox-backup/proxy.key /etc/proxmox-backup/certs.bak
-#cp /root/piplayer/configfiles/wildcards.crt /etc/proxmox-backup/proxy.pem
+cp /root/piplayer/configfiles/wildcards.crt /etc/proxmox-backup/proxy.pem
 #cat ~/uwca/wildcards.key
 #cat >/etc/proxmox-backup/proxy.key
-#systemctl reload proxmox-backup-proxy
+systemctl reload proxmox-backup-proxy
 
 dd if=/dev/zero of=/safemount.img bs=4M count=1
 mkfs.ext4 -L safemount /safemount.img
@@ -68,8 +35,10 @@ mount -L aspace /safemount/aspace/
 
 
 mkfs.ext4 -L space /dev/sda1
-mkdir /space
-mount -L space /space
+
+echo "/safemount.img /safemount ext4 defaults,nofail 0 2" >> /etc/fstab
+echo "LABEL=aspace /safemount/aspace ext4 defaults,nofail 0 3" >> /etc/fstab
+
 
 #ssacli ctrl slot=1 modify dwc=enable forced
 #ssacli ctrl slot=1 modify nbwc=enable
